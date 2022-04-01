dd866ae0350aa76f54d75622d5f1cc2444c86650
Lucas Ultremare <65183236+LucasUltremare@users.noreply.github.com>
2022-04-01 20:12:39 -0300

    exercicio01

---
+++ exercicio01.java
@@ -0,0 +1,30 @@
+package exercicio01;
+
+
+public class exercicio01 {
+
+
+
+	public static void main(String[] args) {
+		
+		double C, F, K , Re, Ra;
+		
+		C = 0;
+		
+		F = C * 1.8 + 32;
+		K = C + 273.15;
+		Re = C * 0.8;
+		Ra = C * 1.8 + 459.67;
+		
+		System.out.println("A Temperatura em Fahrenheit é: " + F);
+		System.out.println("A Temperatura em Kelvin é: " + K);
+		System.out.println("A Temperatura em Réaumur é: " + Re);
+		System.out.println("A Temperatura em Rankine é: " + Ra);
+		
+		
+		
+		
+
+	}
+
+}
