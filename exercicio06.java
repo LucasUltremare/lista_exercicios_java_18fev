151a8084c2825e778001dd233f31a4123c30ac49
Lucas Ultremare <65183236+LucasUltremare@users.noreply.github.com>
2022-04-01 21:56:01 -0300

    Add files via upload

---
+++ exercicio06.java
@@ -0,0 +1,16 @@
+package exercicio01;
+
+public class exercicio06 {
+
+    public static void main(String[] args) {
+        double m, h, t, cavalos;
+        m = 21;
+        h = 2.2;
+        t = 1.4;
+
+        cavalos = (m * h / t) / 745.6999;
+
+        System.out.println("A quantidade de cavalos necessários é " + cavalos);
+    }
+    
+}
